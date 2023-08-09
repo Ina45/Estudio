1d5c4eb1280cb090026af11b9f84233ddb2a8e2c
violeta <evangelina1977@gmail.com>
2023-08-09 11:06:02 -0300

    primer commit

---
+++ lista_familia.py
@@ -0,0 +1,8 @@
+
+Familia=["Josefa", "Mario", "Marcela", "Violeta", "Carolina", "Sebastian"]
+print(Familia)
+
+Familia=["Josefa", "Mario", "Marcela", "Violeta", "Carolina", "Sebastian"]
+Familia.sort()
+print(Familia)
+
